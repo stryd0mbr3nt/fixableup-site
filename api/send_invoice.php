83e47b624cdda025e5fe6ee9a6065a88600242ea
stryd0mbr3nt <112611839+stryd0mbr3nt@users.noreply.github.com>
2025-05-26 10:11:18 +0200

    Update send_invoice.php

--- api/send_invoice.php
+++ api/send_invoice.php
@@ -1,1 +1,18 @@
 
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/autoload.php'; // Make sure you run `composer require phpmailer/phpmailer`
+
+$mail = new PHPMailer(true);
+try {
+    $mail->setFrom('info@fixableup.online', 'FixableUp');
+    $mail->addAddress('client@example.com');
+    $mail->Subject = 'Your Invoice';
+    $mail->Body    = 'Attached is your invoice.';
+    $mail->addAttachment('invoices/invoice123.pdf');
+
+    $mail->send();
+    echo 'Invoice sent';
+} catch (Exception $e) {
+    echo "Mailer Error: {$mail->ErrorInfo}";
+}
